5f11e8903f03e907e9b1cb1d9dac9be2ceeed595
LakshyThecoder <86670450+LakshyThecoder@users.noreply.github.com>
2024-07-17 21:06:50 +0530

    Create Home.js

---
+++ src/Home.js
@@ -0,0 +1,11 @@
+// src/Home.js
+import React from 'react';
+import Spline from '@splinetool/react-spline';
+
+export default function Home() {
+  return (
+    <main>
+      <Spline scene="https://prod.spline.design/AS6sHWbMp5f6xqIp/scene.splinecode" />
+    </main>
+  );
+}
